3159ed68e0047e5f8cf2f6df7316d113028ccb7c
Mathieu Lecarme <mlecarme@bearstech.com>
2015-02-16 11:09:16 +0100

    Fake search.

--- src/grafanastore/app.py
+++ src/grafanastore/app.py
@@ -2,7 +2,10 @@ from flask import Flask, abort, Response, request, jsonify
 import os
 import os.path
 import json
+from glob import glob
+import codecs
 
+from addict import Dict
 
 class GrafanaStore(Flask):
 
@@ -15,8 +18,8 @@ class GrafanaStore(Flask):
 app = GrafanaStore(__name__)
 
 
-@app.route('/dashboard/<dashboard>', methods=['GET', 'PUT'])
-def bashboard(dashboard):
+@app.route('/grafana-dash/dashboard/<dashboard>', methods=['GET', 'PUT', 'DELETE'])
+def dashboard(dashboard):
     path = 'data/%s.json' % dashboard
     if request.method == 'GET':
         if not os.path.exists(path):
@@ -31,6 +34,42 @@ def bashboard(dashboard):
     else:
         abort(400)
 
+
+@app.route('/grafana-dash/dashboard/_search', methods=['POST'])
+def search():
+    """
+    {"query":{"query_string":{"query":"title:*"}},"facets":{"tags":{"terms":{"field":"tags","order":"term","size":50}}},"size":20,"sort":["_uid"]}
+    """
+    q = Dict(json.loads(request.get_data()))
+    if not q.query.query_string.query == "title:*":
+        return jsonify([])
+    else:
+        hits = []
+        for p in glob('data/*.json'):
+            raw = codecs.open(p, 'r', 'utf8').read()
+            j = Dict(json.loads(raw))
+            d = Dict()
+            d._index = 'grafana-dash'
+            d._type = 'dashboard'
+            d._score = None
+            d._id = j.title
+            d._source.title = j.title
+            d._source.user = "guest"
+            d._source.group = "guest"
+            d._source.tags = j.tags
+            d._source.dashboard = raw
+
+            hits.append(d)
+        r = Dict()
+        r.hits.total = len(hits)
+        r.hits.hits = hits
+        r.facets.tags._type = "terms"
+        r.facets.tags.terms = []
+        r.facets.tags.total = 0
+        r.facets.tags.other = 0
+        print r
+        return jsonify(r, mimetype='application/json')
+
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
